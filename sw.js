478ae3f4a483ee9d629d8070c4b9d36b2f4fa2c3
zablehdormant <104545635+zablehdormant@users.noreply.github.com>
2026-04-29 17:48:21 +0600

    Implement caching for assets in service worker
    Added service worker caching for assets during installation.

--- sw.js
+++ sw.js
@@ -1,7 +1,22 @@
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
+const CACHE_NAME = 'poofnotes-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
+];
+
+// Install: Cache all the files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
+// Fetch: Try the network, fall back to cache
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     fetch(event.request).catch(() => {
