22216f49b6a23120b3f9efb34cb71eb710200817
zablehdormant <104545635+zablehdormant@users.noreply.github.com>
2026-04-29 17:13:54 +0600

    Add service worker with install and fetch event handlers

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
